6bd0db54b8b7731df7ab3a79cd08e83d854c3cae
Prashant Varanasi <prashant@prashantv.com>
2012-03-21 02:52:14 -0700

    Support for getting last URLs as well

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 var io = require('socket.io').listen(8090);
 
+var lastUrls = []
+
 io.sockets.on('connection', function (socket) {
   socket.on('newUrl', function (data) {
     console.log(data);
     io.sockets.emit('newUrl', data);
+    if (lastUrls.length > 9) lastUrls.length = 9;
+    lastUrls.push(data.url);
   });
-
+  socket.on('lastUrls', function(data) {
+    console.log('lastURL request');
+    socket.emit('lastUrls', { urls: lastUrls });
+  });
 });
 

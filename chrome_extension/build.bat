62f09bfd93f180a4658b19784e6a508b59d37d08
U-Charles-PC\Charles <Charles@Charles-PC.(none)>
2012-03-29 02:35:04 -0700

    windows chrome exension build script. + code cleanup

---
+++ chrome_extension/build.bat
@@ -0,0 +1,6 @@
+pushd ..
+set extensiondir=%CD%
+chrome --pack-extension=%extensiondir%\chrome_extension --pack-extension-key=%extensiondir%\chrome_extension.pem
+
+mv chrome_extension.crx server\airtab.crx
+popd 
--- chrome_extension/build.sh
+++ chrome_extension/build.sh
@@ -1,9 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 working_dir=`pwd`
 pushd ..
 echo $working_dir
-
 version=`grep 'version' $working_dir/manifest.json | tr '"' '\n' |grep '[0-9]'`
-
 zip -r $working_dir/airtab-$version.zip $working_dir -x \*.git\* -x \*.swp -x \*.zip                              
+#chrome --pack-extension="$working_dir" --pack-extension-key=chrome_extension.pem
 popd
